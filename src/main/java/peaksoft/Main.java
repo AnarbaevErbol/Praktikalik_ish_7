178aae47473e176601c95734760d1499f54a3d7f
AnarbaevErbol <anarbaeverik96@gmail.com>
2022-04-21 13:53:41 +0300

    hm-7

--- src/main/java/peaksoft/Main.java
+++ src/main/java/peaksoft/Main.java
@@ -1,7 +1,20 @@
 package peaksoft;
 
+import peaksoft.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        UserServiceImpl userService = new UserServiceImpl();
+//       userService.createUsersTable();
+//       userService.saveUser("Erbol", "Anarbaev",(byte) 20);
+//       userService.saveUser("Edil", "Munarbekov",(byte) 18);
+//       userService.saveUser("Aziz", "Kerimov",(byte) 23);
+//       userService.saveUser("Asan", "Taalaybekov",(byte) 19);
+        userService.getAllUsers().forEach(System.out::println);
+        //userService.cleanUsersTable();
+        //userService.dropUsersTable();
+       System.out.println(userService.existsByFirstName("Edil" +
+               ""));
     }
 }
